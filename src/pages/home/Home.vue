f1a678581019e212d0a29161ddbe45da1cc76a0f
HJason1220 <heyaxin.789@gmail.com>
2018-06-30 19:47:12 +0800

    header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<home-header></home-header>
+	</div>
+</template>
+
+<script>
+import HomeHeader from './com/Header'
+export default{
+	name:"Home",
+	components:{
+		HomeHeader
+	}
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/home/com/Header.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+	<div class="header">
+		<div class="header-left">
+			<div class="iconfont back-icon">&#xe600;</div>
+		</div>
+		<div class="header-input">
+			<span class="iconfont">&#xe6e4;</span>
+			输入城市/景点/游玩主题
+		</div>
+		<div class="header-right">
+			城市
+			<span class="iconfont arrow-icon">&#xe64a;</span>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+	export default{
+		name:'HomeHeader'
+	}
+</script>
+
+<style lang='scss' scoped="scoped">
+@import '~styles/varible.scss';
+.header{
+	line-height: 0.86rem;
+	display:flex;
+	background:$bgColor;
+	color:#fff;
+	
+	.header-left{
+		width: 0.64rem;
+		float:left;
+		.back-icon{
+			text-align: center;
+			font-size:.4rem;
+		}
+	}
+	.header-input{
+		flex:1;
+		height:.64rem;
+		margin-top: .12rem;
+		margin-left:.2rem;
+		background: #fff;
+		padding-left: .2rem;
+		border-radius: 0.1rem;
+		line-height: .64rem;
+		color: #ccc;
+	}
+	.header-right{
+		width: 1.24rem;
+		float:right;
+		text-align: center;
+		.arrow-icon{
+			font-size:.24rem;
+			margin-left: -0.04rem;
+		}
+	}
+}
+</style>
